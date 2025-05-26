ddfe180aa9bda61dfb2914730a09c8c27664f3bf
han-yerim <eternity7236@naver.com>
2025-05-26 23:24:33 +0900

    feat : 마우스 커서
    기본 마우스 커서 변경

---
+++ 2sThisReaL/Assets/Scripts/UI/MouseCursor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+    public Texture2D customCursor;
+    public Vector2 hotSpot = Vector2.zero;
+    public CursorMode cursorMode = CursorMode.Auto;
+
+    void Start()
+    {
+        Cursor.SetCursor(customCursor, hotSpot, cursorMode);
+    }
+
+    void Update()
+    {
+        
+    }
+}
