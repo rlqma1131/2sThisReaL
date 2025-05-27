84f56fc509f65eea6e4e40a40776926f66b4df30
Hicanos <ruda32168@gmail.com>
2025-05-27 12:56:27 +0900

    feat; Item관련 cs들 추가
    feat; Item관련 cs들 추가

---
+++ 2sThisReaL/Assets/Scripts/GameObj/ResourceItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ResourceItem", menuName = "New Item/ResourceItem", order = 1)]
+public class ResourceItem : ItemData
+{
+   
+    // 자원 아이템은 제작에서 사용되는 재료로 분류
+    // 특별한 기능이 없는 자원 아이템은 ItemType에 Resource만 설정
+}
