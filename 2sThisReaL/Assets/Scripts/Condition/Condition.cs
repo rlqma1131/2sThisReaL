9da4d2d80cee618096488135044124870d69498b
ChaWooJin <chawoojin06@gmail.com>
2025-05-27 11:40:04 +0900

    feat_codeUpdate

--- 2sThisReaL/Assets/Scripts/Condition/Condition.cs
+++ 2sThisReaL/Assets/Scripts/Condition/Condition.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Condition : MonoBehaviour
 {
+    [SerializeField]
+    private ConditionManager _conditionManager;
+
+    [SerializeField] private Image _imageHp;
+    [SerializeField] private Image _imageStamina;
+    [SerializeField] private Image _imageHunger;
+    [SerializeField] private Image _imageThirsty;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _conditionManager.curHp = _conditionManager.maxHp;
+        _conditionManager.curStamina = _conditionManager.maxStamina;
+        _conditionManager.curHunger = _conditionManager.maxHunger;
+        _conditionManager.curThirsty = _conditionManager.maxThirsty;
     }
-
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    #region HP
+    public void DeltaHP(float delta)
+    {
+
+    }
+    #endregion
 }
--- 2sThisReaL/Assets/Scripts/Condition/ConditionManager.cs
+++ 2sThisReaL/Assets/Scripts/Condition/ConditionManager.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class ConditionManager : MonoBehaviour
 {
+    [Header("HP")]
+    public float curHp;
+    public float maxHp;
+    public float decreasingHP;
+
+    [Header("Stamina")]
+    public float curStamina;
+    public float maxStamina;
+    public float decreasingStamina;
+
+    [Header("Hunger")]
+    public float curHunger;
+    public float maxHunger;
+    public float decreasingHunger;
+
+    [Header("Thirsty")]
+    public float curThirsty;
+    public float maxThirsty;
+    public float decreasingThirsty;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
