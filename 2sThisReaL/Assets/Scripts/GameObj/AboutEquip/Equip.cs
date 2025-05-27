22d650e8ef65b9e551587a86582c6ba4d77767d5
Hicanos <ruda32168@gmail.com>
2025-05-27 19:05:43 +0900

    add; Layer , Scripts
    Interactable
    Resource
    Equip
    스크립트도 몇 개 추가됐습니다.

---
+++ 2sThisReaL/Assets/Scripts/GameObj/AboutEquip/Equip.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equip : MonoBehaviour
+{
+    public virtual void OnAttackInupt()
+    {
+
+    }
+}
---
+++ 2sThisReaL/Assets/Scripts/GameObj/AboutEquip/EquipTool.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipTool : Equip
+{
+    // 캐릭터가 도구를 사용할 때 액션 등의 처리
+
+
+
+}
