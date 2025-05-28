47c452fae68e3c100c90231f37f0be6618db44ba
Hicanos <ruda32168@gmail.com>
2025-05-28 19:58:20 +0900

    feat; Resources Prefab, SnowTree 1 프리펩
    Resources.Prefab의 하위에 SnowTree1 과 같은 프리펩을 넣으면, 하위의 ResourceObject 스크립트 내에서 Respawn Time을 조절하여 각 개체별로 리스폰 시간을 다르게 만들 수 있도록 해두었습니다.

--- 2sThisReaL/Assets/Scripts/GameObj/EquipItem.cs
+++ 2sThisReaL/Assets/Scripts/GameObj/EquipItem.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GatherType
+{
+    //벌목, 채광
+    Lumbering,//벌목 -목재류 수급
+    Mining // 채광 - 광물 수급
+}
 
 [CreateAssetMenu(fileName = "EquipableItem", menuName = "New Item/Equipable")]
 public class EquipItem : ItemData
 {
     [Header("장비 설정")]
-    public bool isEquipable; // 장비 여부 (기본값 false)
+    [SerializeField] public bool isEquipable; // 장비 여부 (기본값 false)
     // 장비 아이템은 도구와 무기로 분류, 도구라도 일단 '공격'은 가능함
     public GameObject equipItem; // 장착할 때 사용할 프리팹 (선택 사항, 도구나 무기 아이템에 사용)
     public bool canGathering; // 자원 채집 가능 여부 (true면 해당하는 아이템을 채집 가능)
+    public GatherType getType; // 자원 채집 타입 (벌목, 채광 등)
     public int attackPower; // 무기가 아니라도 공격력이 있는 아이템은 공격력 설정 가능 (기본값 0)
 
     [Header("공격 설정")]
---
+++ 2sThisReaL/Assets/Scripts/GameObj/ResourceControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceControl : MonoBehaviour
+{
+    //ResourceControl은 ResourceObject를 관리하는 스크립트
+    //ResourceObject의 자원이 0이 되었을 때, 일정 시간이 지나면 다시 활성화 시켜야 함
+
+    public static ResourceControl Instance { get; private set; } // 싱글톤 인스턴스
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void RequestRespawn(ResourceObject resource, float delay)
+    {
+        // 자원 오브젝트를 비활성화하고 일정 시간 후에 다시 활성화
+        StartCoroutine(RespawnCoroutine(resource, delay));
+    }
+
+    IEnumerator RespawnCoroutine(ResourceObject resource, float delay)
+    {
+        //시간 측정 디버그
+
+        yield return new WaitForSeconds(delay);
+        resource.Initialize(); // 자원 초기화
+        resource.gameObject.SetActive(true); // 자원 오브젝트 활성화
+
+    }
+
+    // 여기서 만약 IsTrigger 콜라이더에 Tag= Player나 Enemy가 있다면, resource.gameObject.SetActive를 true로 하지 않고 추가 지연을 시킴
+    // => 별도의 스크립트로 빼기
+
+}
