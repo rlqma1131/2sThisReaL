a4ed5910c13c84fc550205f3bbc8b984bdc52f3e
rlqma1131 <rlqma1131@gmail.com>
2025-05-29 20:41:49 +0900

    chore: 줄 변경

--- 2sThisReaL/Assets/Scripts/UI/UIInventory.cs
+++ 2sThisReaL/Assets/Scripts/UI/UIInventory.cs
@@ -31,8 +31,8 @@ public class UIInventory : MonoBehaviour
     void Start()
     {
         controller = GameManager.Instance.Player.controller;
-        Condition = ConditionManager.Instance.Condition;
         dropPosition = GameManager.Instance.Player.dropPosition;
+        Condition = ConditionManager.Instance.Condition;
         GameManager.Instance.Player.additem += AddItem;
 
         inventoryWindow.SetActive(false);
