8fba29229b15e75f103a463edd10362f1aea59a1
rlqma1131 <rlqma1131@gmail.com>
2025-05-29 09:27:31 +0900

    fix: GameManager 수정

--- 2sThisReaL/Assets/Scripts/GameManager.cs
+++ 2sThisReaL/Assets/Scripts/GameManager.cs
@@ -1,10 +1,22 @@
-﻿public class GameManager
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
 {
-    private static GameManager instance;
-    public static GameManager Instance => instance ??= new GameManager();
+    public static GameManager Instance { get; private set; }
 
     public Player Player { get; set; }
     public Enemy Enemy { get; set; }
 
-    private GameManager() { }
+    private void Awake()
+    {
+        // 싱글톤 패턴
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject); // 중복 제거
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject); // 씬 이동 시 유지하고 싶으면
+    }
 }
