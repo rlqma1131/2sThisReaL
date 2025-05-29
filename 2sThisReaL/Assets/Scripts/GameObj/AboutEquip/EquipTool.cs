f493916ff3316656821e2e793fd1c5bc969a0592
Hicanos <ruda32168@gmail.com>
2025-05-29 19:28:14 +0900

    feat; Ray를 이용해 자원 채집
    Player.Prefabs에 변경점이 있어 NewPlayer를 신설했습니다.

--- 2sThisReaL/Assets/Scripts/GameObj/AboutEquip/EquipTool.cs
+++ 2sThisReaL/Assets/Scripts/GameObj/AboutEquip/EquipTool.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EquipTool : Equip
 {
@@ -8,76 +6,75 @@ public class EquipTool : Equip
     // 도구에는 Collider를 이용, IsTrigger Enter가 되면 공격 혹은 자원 채집이 됨 (Layer = Resorce면 자원이, Layer = Enemy면 공격이 됨)
     // 도구에 대한 내용은 EquipItem에서 설정하고 이곳에 받아옴
 
-    public EquipItem equipData; // 도구 아이템 데이터
-    private bool attacking; // 공격 중인지 여부. true면 공격이 실행되지 않음. - 애니메이션 쿨이 다 돌 때까지는 attcking 상태.
+    [SerializeField] private EquipItem equipData; // 도구 아이템 데이터
     private PlayerController controller; // 플레이어 컨트롤러 참조, 컨트롤러의 애니메이션이 진행되는 것= 공격임
-    private Player player; //플레이어 참조. 플레이어의 현재 장비 확인
+    // transform을 참조할 Player 불러오기
+    private Player player;
+    private Ray ray; // 도구가 장착된 오브젝트의 위치에서 시작하는 Ray
 
     private void Awake()
     {
         controller = GetComponentInParent<PlayerController>();
+        player = GetComponentInParent<Player>(); // 플레이어 컴포넌트 참조
+    }
+
+    private void Start()
+    {
+        //어차피 상호작용 중인 EquipTool = 해당 스크립트가 붙은 오브젝트임
+        // 도구 아이템 데이터가 반드시 설정되어 있으므로, 해당 아이템 Data에서 Distance에 따른 Raycast를 사용하여 자원/적을 탐지
+        if (equipData == null)
+        {
+            Debug.LogError("EquipTool: equipData is not set!");
+            return;
+        }
+
     }
     public override void OnAttackInput()
     {
-        // 도구 사용 로직 처리
-        if (!attacking)
-        {
-            attacking = true;           
-            // 공격이 시작되면 애니메이션을 실행-플레이어의 공격로직
-            controller.OnAttack(default);
-        }
+        CheckTarget();
+        
     }
 
+    //공격 시 자원/적을 탐지하는 메서드
     //콜라이더랑 부딪히면 - 자원 채집 혹은 공격
     //Layer = Resource면 자원 채집, Layer = Enemy면 공격
     //공격 키를 눌렀을 때 캐릭터의 전방으로 Raycast 혹은 BoxCast를 쏴서 전방에 자원/적이 있는지 확인 ->있다면 자원을 얻거나 적을 공격
     //Raycast를 구하는 메서드를 생성
 
-
-    private void Start()
+    private void CheckTarget()
     {
-        player = GetComponentInParent<Player>();
-        //플레이어가 현재 장착하고 있는 도구 아이템을 가져옴
-        
-    }
+        //Raycast를 통해 탐지한 자원/ 적에 대한 처리
+        //Ray는 해당 Tool이 장착된 오브젝트의 위치에서 시작. 해당 오브젝트의 전방 방향으로 Ray를 쏘기
+        // ray의 origin을 Player의 위치로 설정
+        ray = new Ray(player.transform.position + Vector3.up * 1.5f, player.transform.forward); // 플레이어의 위치에서 시작, 위치를 약간 위로
+        RaycastHit hit;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Resource") && equipData.canGathering)
+        // 도구의 공격 거리 내에 자원이나 적이 있는지 확인
+        if (Physics.Raycast(ray, out hit, equipData.attackDistance))
         {
-            // 자원 채집 로직 처리
-            Debug.Log("자원 채집: " + other.name);
-            ResourceObject resource = other.GetComponent<ResourceObject>();
-            if (resource != null)
+            // 자원 채집
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Resource") && equipData.canGathering)
             {
-                // 자원 획득
-                resource.Gather(transform.position, transform.forward);
+                Debug.Log("자원 채집: " + hit.collider.name);
+                ResourceObject resource = hit.collider.GetComponent<ResourceObject>();
+                if (resource != null)
+                {
+                    // 자원 획득
+                    resource.Gather(hit.point, hit.normal); // 자원 채집 메서드 호출
+                }
             }
-            // 자원 채집 관련 코드 추가
-        }
-        else if (other.CompareTag("Enemy") && equipData.attackPower > 0)
-        {
-            //에너미는 layer가 아니라 tag로 구분
-            // 공격 로직 처리
-            Debug.Log("공격: " + other.name);
-            Enemy enemy = other.GetComponent<Enemy>();
-            if (enemy != null)
+            // 적 공격
+            else if (hit.collider.CompareTag("Enemy") && equipData.attackPower > 0)
             {
-                // 적에게 피해를 입힘
-                enemy.TakeDamage(equipData.attackPower);
+                Debug.Log("공격: " + hit.collider.name);
+                Enemy enemy = hit.collider.GetComponent<Enemy>();
+                if (enemy != null)
+                {
+                    // 적에게 피해를 입힘
+                    enemy.TakeDamage(equipData.attackPower);
+                }
             }
-            // 공격 관련 코드 추가
         }
-
-        //공격이 끝나면 attacking을 false로 변경
-        OncanAttack();
     }
-
-
-    void OncanAttack()
-    {
-        attacking = false; // 공격이 끝나면 attacking을 false로 설정
-    }
-
 
 }
