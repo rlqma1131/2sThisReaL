44bc37163b6c347ecabc81b05f8b318ecb2cbaf1
Hicanos <ruda32168@gmail.com>
2025-05-31 03:38:43 +0900

    add; CraftUI
    제작용 UI틀 완성.

---
+++ 2sThisReaL/Assets/Scripts/Crafting/CraftSlot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CraftSlot : MonoBehaviour
+{
+    [SerializeField] private Image iconImage;
+    [SerializeField] private TextMeshProUGUI countText;
+    [SerializeField] private Button selectButton;
+
+    private RecipeBase recipeData;
+    private int slotIndex; // 슬롯의 순서
+    private bool isAvailable; // 제작 가능한 아이템이면 Alpha 1, 제작 불가능한 아이템이면 Alpha 0.4로 표시
+
+
+
+    //Slot을 선택하면 다른 슬롯에 필요한 아이템 슬롯을 만들어 인스턴스
+    //각 슬롯에는 아이템의 Icon과 개수를 보여주며, 버튼을 눌러 선택
+    //선택된 아이템 유형 메뉴에 따라 나오는 슬롯이 바뀌어야함(BuileItem은 BuildItem들 제작법만, CookRecipe는 요리법만 등)
+
+    public void SelectRecipe()
+    {
+
+    }
+
+}
--- 2sThisReaL/Assets/Scripts/Crafting/CraftingSystem.cs
+++ 2sThisReaL/Assets/Scripts/Crafting/CraftingSystem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CraftingSystem : MonoBehaviour
 {
@@ -9,10 +10,23 @@ public class CraftingSystem : MonoBehaviour
     // 레시피는 각각에 해당하는 것을 가져옴. BuiildCraftRecipe, CookingRecipe 등
     // 빌드 데이터는 데이터상으로만 추가되지만, 도구 제작 등은 완성 후 인벤토리에 추가되어야 함.
     // 외부에서 선택된 레시피를 받아와서 처리한다.
+    // 제작하기 버튼을 누르면 결과물을 정해진 곳에 추가한다. (BuildCraftRecipe는 ResourceManager에 데이터를 추가, 나머지는 인벤토리에 추가)
+    // 레시피를 받으면 그만큼 UI에 아이템 슬롯을 만들어서 보여준다.
 
+    [Header("제작 레시피 설정")]
     public List<RecipeBase> recipes; // 크래프팅 레시피 목록
     public List<BuildCraftRecipe> buildCraftRecipes; // 빌드 크래프트 레시피 목록
     public RecipeBase curRecipe; // 현재 선택된 레시피
+
+    [Header("UI Slot 설정")]
+    //RecipeSlot을 할당할 부모 컴포넌드
+    public Transform recipeSlotParent;
+
+    //재료를 할당할 부모 컴포넌트
+    public Transform ResourcesParent;
+
+    [Header("결과물 아이콘")]
+    [SerializeField] private Image resultIcon; // 결과물 아이콘
 
 
     public void MatchRecipe()
