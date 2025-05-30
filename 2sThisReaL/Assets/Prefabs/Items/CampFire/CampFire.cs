a56cbf47684589de3688fb17927b7178dc991af5
DounPark <ehdns1562@gmail.com>
2025-05-30 16:01:21 +0900

    add 또캠파 fix house
    add 또캠파 fix house

---
+++ 2sThisReaL/Assets/Prefabs/Items/CampFire/CampFire.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CampFire : MonoBehaviour
+{
+    // public int damage;
+    // public float damageRate;
+    //
+    // List<IDamageable> things = new List<IDamageable>();
+    // void Start()
+    // {
+    //     InvokeRepeating("DealDamage", 0, damageRate);   // �ٷ� �����ؼ� damageRate �������� �ݺ� ȣ��
+    // }
+    //
+    // void DealDamage()
+    // {
+    //     for(int i = 0; i < things.Count; i++)
+    //     {
+    //         things[i].TakePhysicalDamage(damage);
+    //     }
+    // }
+    //
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     if(other.TryGetComponent(out IDamageable damageable)) // IDamageable ������Ʈ�� ���� �ִٸ� �浹���� �� things ����Ʈ�� �߰�
+    //     {
+    //         things.Add(damageable);
+    //     }
+    // }
+    //
+    // private void OnTriggerExit(Collider other)  // IDamageable ������Ʈ�� ���� �ִٸ� �浹�� ������ �� things ����Ʈ���� ����
+    // {
+    //     if(other.TryGetComponent(out IDamageable damageable))
+    //     {
+    //         things.Remove(damageable);
+    //     }
+    // }
+}
