d686e393c2f56cbf86d489233508482730c8449c
Hicanos <ruda32168@gmail.com>
2025-05-30 23:13:09 +0900

    fix; Branch 나뭇가지가 잃어버린 몸을 찾았습니다

--- 2sThisReaL/Assets/Scripts/Crafting/CraftTool.cs
+++ 2sThisReaL/Assets/Scripts/Crafting/CraftTool.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class CraftTool : MonoBehaviour, IInteractable
 {
+
     //제작대에 다가가서 상호작용을 하면 
     public string GetInteractPrompt()
     {
-        throw new System.NotImplementedException();
+        return $"제작대와 상호작용 하려면 [E]를 눌러주세요."; //상호작용 프롬프트
     }
 
     public void OnInteract()
