7976a1235389276e9db8f610c04eae1ad664f625
Hicanos <ruda32168@gmail.com>
2025-05-31 00:02:45 +0900

    feat; 제작대.cs
    제작대cs 제작 중

--- 2sThisReaL/Assets/Scripts/Crafting/CraftTool.cs
+++ 2sThisReaL/Assets/Scripts/Crafting/CraftTool.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class CraftTool : MonoBehaviour, IInteractable
 {
+    [SerializeField] private GameObject craftingUI; //제작 UI 오브젝트
+    private bool craftMode;
+
+    [Header("마우스 커서")]
+    [SerializeField] private Texture2D craftingCursor; //제작커서
+
+    private void Start()
+    {        
+        craftMode = false; //초기 제작 모드 비활성화
+    }
 
     //제작대에 다가가서 상호작용을 하면 
     public string GetInteractPrompt()
@@ -14,6 +24,7 @@ public class CraftTool : MonoBehaviour, IInteractable
     public void OnInteract()
     {
         //제작대 상호작용> 제작 UI 띄우기
-
+        //상호작용 시 UI를 활성화. 다시 한 번 사용 시 UI 비활성. UI가 활성화 되는 동안 캐릭터는 이동 금지
+        
     }
 }
