6756b9078b7175d149bccf3d9d5ca2b44fa52849
StarCandy-D2 <kdh04090@gmail.com>
2025-05-31 22:48:23 +0900

    fix HES

---
+++ 2sThisReaL/Assets/Scripts/EndingScene/HappyEndingInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HappyEndingInitializer : MonoBehaviour
+{
+    void Start()
+    {
+
+        //커서 활성화
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
+
