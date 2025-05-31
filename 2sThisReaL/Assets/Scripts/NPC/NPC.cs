04ee159bcae815e1c536346fdf882e32163df278
rlqma1131 <rlqma1131@gmail.com>
2025-05-31 16:03:05 +0900

    feat: NPC 아이템 전달 구현

---
+++ 2sThisReaL/Assets/Scripts/NPC/NPC.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC Item")]
+    [SerializeField] private ItemData[] itemData;
+
+    private Animator anim;
+    private Collider npcCollider;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        npcCollider = GetComponent<Collider>();
+    }
+    public void OnRobbery() // 강탈했을 때 호출해주세용
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            if (itemData[i] != null)
+            {
+                GameManager.Instance.Player.itemData = itemData[i];
+                GameManager.Instance.Player.additem?.Invoke();
+            }
+        }
+    }
+
+    public void OnTalk() // 대화했을 때 호출해주세용
+    {
+        for(int i = 0; i < itemData.Length; i++)
+        {
+            if (itemData[i] != null)
+            {
+                GameManager.Instance.Player.itemData = itemData[i];
+                GameManager.Instance.Player.additem?.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (anim != null)
+            {
+                anim.SetBool("Moving", true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (anim != null)
+            {
+                anim.SetBool("Moving", false);
+            }
+        }
+    }
+}
