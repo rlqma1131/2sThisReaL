7737999495ac289ef6bc60c1dd442a03bff3879c
StarCandy-D2 <kdh04090@gmail.com>
2025-05-31 20:34:35 +0900

    Add SkillCoolDownUI

--- 2sThisReaL/Assets/Scripts/Skills/Skill.cs
+++ 2sThisReaL/Assets/Scripts/Skills/Skill.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [Serializable]
 public class Skill
@@ -12,6 +13,8 @@ public class Skill
 
     [HideInInspector] public float lastUsedTime;
 
+    [Header("Optional UI")]
+    public Image cooldownImage;
     public bool IsReady()
     {
         return Time.time >= lastUsedTime + cooldown;
@@ -20,5 +23,13 @@ public class Skill
     public void Use()
     {
         lastUsedTime = Time.time;
+    }
+    public void UpdateCooldownUI()
+    {
+        if (cooldownImage == null) return;
+
+        float elapsed = Time.time - lastUsedTime;
+        float ratio = Mathf.Clamp01(elapsed / cooldown);
+        cooldownImage.fillAmount = 0f + ratio;
     }
 }
